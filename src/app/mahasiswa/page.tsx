10471a448d445b3192b03c4df09ecee0bd9fd08a
akhdanrgya <akhdan.anargya@gmail.com>
2023-12-24 17:36:27 +0700

    fixing

--- src/app/mahasiswa/page.tsx
+++ src/app/mahasiswa/page.tsx
@@ -1,38 +1,35 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-const getMahasiswa =async () => {
-    const res = await prisma.mahasiswa.findMany({
-        select:{
-            nama: true,
-            nim: true,
-            kelas: true
-        }
-    })
-    return res
-}
+const getMahasiswa = async () => {
+  const res = await prisma.mahasiswa.findMany({
+    select: {
+      nama: true,
+      nim: true,
+      kelas: true,
+    },
+  });
+  return res;
+};
 
-
-const Mahasiswa = async() => {
-    const mahasiswa = await getMahasiswa()
+const Mahasiswa = async () => {
+  const mahasiswa = await getMahasiswa();
   return (
     <div>
-        <h1>
-            Mahasiswa Page
-        </h1>
+      <h1>Mahasiswa Page</h1>
 
-        <div>
-            {mahasiswa.map((data,index) => (
-                <>
-                <p key={data.nama}>{index + 1}</p>
-                <p>{data.nama}</p>
-                <p>{data.nim}</p>
-                <p>{data.kelas}</p>
-                </>
-            ))}
-        </div>
+      <div>
+        {mahasiswa.map((data, index) => (
+          <div>
+            <p key={data.nama}>{index + 1}</p>
+            <p>{data.nama}</p>
+            <p>{data.nim}</p>
+            <p>{data.kelas}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mahasiswa
+export default Mahasiswa;
