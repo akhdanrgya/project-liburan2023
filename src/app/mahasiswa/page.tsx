b8ff63bf1c7619ba84757b4dab0f883ed2a5475c
akhdanrgya <akhdan.anargya@gmail.com>
2023-12-24 17:31:12 +0700

    update fetching data dari db

--- src/app/mahasiswa/page.tsx
+++ src/app/mahasiswa/page.tsx
@@ -1,7 +1,37 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
 
-const Mahasiswa = () => {
+const getMahasiswa =async () => {
+    const res = await prisma.mahasiswa.findMany({
+        select:{
+            nama: true,
+            nim: true,
+            kelas: true
+        }
+    })
+    return res
+}
+
+
+const Mahasiswa = async() => {
+    const mahasiswa = await getMahasiswa()
   return (
-    <div>mahasiswa page</div>
+    <div>
+        <h1>
+            Mahasiswa Page
+        </h1>
+
+        <div>
+            {mahasiswa.map((data,index) => (
+                <>
+                <p key={data.nama}>{index + 1}</p>
+                <p>{data.nama}</p>
+                <p>{data.nim}</p>
+                <p>{data.kelas}</p>
+                </>
+            ))}
+        </div>
+    </div>
   )
 }
 
