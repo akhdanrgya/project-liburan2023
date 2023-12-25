7b4011a6c4a3d3592ad0aeda3bcd3db4a54a2607
akhdanrgya <akhdan.anargya@gmail.com>
2023-12-26 03:15:36 +0700

    Update Navigation to Dashboard

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -33,12 +33,15 @@ const Navbar = () => {
               <li>
                 <Link href="">About</Link>
               </li>
+
               <li>
                 <Link href="/dashboard">Dashboard</Link>
               </li>
+
               <li>
                 <Link href="">Services</Link>
               </li>
+              
               <li>
                 <Link href="">Contact</Link>
               </li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,21 @@ const Home = () => {
   return (
     <div className="header">
       <Navbar />
-      <div className="h-[100vh] flex justify-center items-center flex-col text-center">
+      <div className="h-[100vh] flex justify-center items-center flex-col text-center p-10">
         <h2 className="text-[28px] font-bold mb-[25px]">
-          testing banget inimah asli
+          Soto Betawi Aselole
         </h2>
         <p className="text-[gray] mb-[15px]">
-          Paragraph aselole asoy geboy anjay juga
+          Inimah deskripsi singkat buat soto betawi aje yekan
+          nanti di belakang text ini ada foto foto nya yakan
         </p>
         <button className="bg-white w-[150px] h-[45px] rounded-[5px] text-[black]">
-          Learn More
+          Learn More 
         </button>
       </div>
 
       <div className="bg-[white] text-[black] p-10 h-screen">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <h1>Pokoknya mah ini buat catalog soto betawi njay</h1>
       </div>
     </div>
   );
