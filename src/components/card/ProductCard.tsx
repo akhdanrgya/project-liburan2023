757d55656449242bd38faabfa33f0f133f293a34
akhdanrgya <akhdan.anargya@gmail.com>
2023-12-26 16:18:55 +0700

    Mapping Product

--- src/components/card/ProductCard.tsx
+++ src/components/card/ProductCard.tsx
@@ -1,4 +1,15 @@
-const ProductCard = () => {
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+const getProduct = async() => {
+  const res = await prisma.product.findMany()
+  
+  return res
+}
+
+const ProductCard = async() => {
+  const product = await getProduct()
+
   return (
     <div className="bg-white">
   <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -6,25 +17,26 @@ const ProductCard = () => {
 
     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
       {/* mapping from api */}
-      <div className="group relative">
+      {product.map((items) => (
+        <div className="group relative" key={items.id}>
         <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="h-full w-full object-cover object-center lg:h-full lg:w-full"/>
+          <img src={items.image} alt="Soto Image" className="h-full w-full object-cover object-center lg:h-full lg:w-full"/>
         </div>
         <div className="mt-4 flex justify-between">
           <div>
             <h3 className="text-sm text-gray-700">
               <a href="#">
                 <span aria-hidden="true" className="absolute inset-0"></span>
-                Basic Tee
+                {items.name}
               </a>
             </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
+            <p className="mt-1 text-sm text-gray-500">{items.Desc}</p>
           </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
+          <p className="text-sm font-medium text-gray-900">Rp.{items.price}</p>
         </div>
       </div>
+))}
 
-      {/* <!-- More products... --> */}
     </div>
   </div>
 </div>
